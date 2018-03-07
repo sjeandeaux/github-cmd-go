11fa357a65320e55e8884f285e0ba923ef97626e
Stéphane Jeandeaux <stephane.jeandeaux@gmail.com>
2018-03-07 11:47:06 -0500

    test

---
+++ internal/os/os_test.go
@@ -0,0 +1,39 @@
+package os
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetenv(t *testing.T) {
+
+	os.Setenv("set-internal-os-key", "in env")
+	defer func() {
+		os.Setenv("set-internal-os-key", "")
+	}()
+
+	var inputs = []struct {
+		key          string
+		defaultValue string
+		expected     string
+	}{
+		{
+			key:          "set-internal-os-key",
+			defaultValue: "defaultValue",
+			expected:     "in env",
+		},
+		{
+			key:          "not-set-internal-os-key",
+			defaultValue: "defaultValue",
+			expected:     "defaultValue",
+		},
+	}
+
+	for _, data := range inputs {
+		actual := Getenv(data.key, data.defaultValue)
+		assert.Equal(t, data.expected, actual)
+	}
+
+}
